f7163f4e53b9ffa90a5217a9d29820bb6633e198
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-27 12:50:08 +0100

    Ex 04

---
+++ 04 - saluti/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+// Dichiara la funzione qui.
+let nome = 'Giulia'
+
+function hello (name) {
+    console.log ('ciao', name)
+}
+
+hello(nome)
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
